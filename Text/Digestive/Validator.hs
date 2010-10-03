1e209f6409673314dc2944f79a96982f09fbfbba
Jasper Van der Jeugt <jaspervdj@gmail.com>
2010-10-03 09:34:43 +0200

    Add `check` and `checkM` functions

--- Text/Digestive/Validator.hs
+++ Text/Digestive/Validator.hs
@@ -17,20 +17,38 @@ instance Monad m => Monoid (Validator m inp) where
     v1 `mappend` v2 = Validator $ \inp ->
         liftM2 mappend (unValidator v1 inp) (unValidator v2 inp)
 
--- | Attach a 'Validator' to a 'Form'
+-- | Attach 'Validator's to a 'Form'
 --
-validate :: Monad m => Form m inp v a -> Validator m a -> Form m inp v a
-validate form validator = Form $ do
-    (view, result) <- unForm form
+validate :: Monad m => Form m inp v a -> [Validator m a] -> Form m inp v a
+validate form validators = Form $ do
+    (view', result) <- unForm form
     range <- getFormRange
     -- Check the result
     case result of
         -- We already have an error, no more validation needed
-        Error e -> return (view, Error e)
+        Error e -> return (view', Error e)
         -- Allright, now apply the validator
         Ok x -> do
-            r <- lift $ lift $ unValidator validator x
+            r <- lift $ lift $ unValidator (mconcat validators) x
             return $ case r of
-                [] -> (view, Ok x)
+                [] -> (view', Ok x)
                 -- Attach the range information to the errors
-                e -> (view, Error $ map ((,) range) e)
+                e -> (view', Error $ map ((,) range) e)
+
+-- | Easy way to create a pure validator
+--
+check :: Monad m
+      => String         -- ^ Error message
+      -> (a -> Bool)    -- ^ Actual validation
+      -> Validator m a  -- ^ Resulting validator
+check error' = checkM error' . (return .)
+
+-- | Easy way to create a monadic validator
+--
+checkM :: Monad m
+       => String         -- ^ Error message
+       -> (a -> m Bool)  -- ^ Actual validation
+       -> Validator m a  -- ^ Resulting validator
+checkM error' f = Validator $ \x -> do
+    valid <- f x
+    return $ if valid then [] else [error']
