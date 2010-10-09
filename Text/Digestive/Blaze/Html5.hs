b2e9992fc41c26a28ac32b3074b642f016e2df25
Jasper Van der Jeugt <jaspervdj@gmail.com>
2010-10-09 19:51:54 +0200

    Add radio selection

--- Text/Digestive/Blaze/Html5.hs
+++ Text/Digestive/Blaze/Html5.hs
@@ -2,8 +2,9 @@
 module Text.Digestive.Blaze.Html5 where
 
 import Control.Applicative ((<$>))
-import Control.Monad (mplus, forM_, unless)
+import Control.Monad (mplus, forM_, unless, when)
 import Data.Maybe (fromMaybe)
+import Data.Monoid (mempty)
 
 import Text.Blaze.Html5 (Html, (!))
 import qualified Text.Blaze.Html5 as H
@@ -12,10 +13,16 @@ import qualified Text.Blaze.Html5.Attributes as A
 import Text.Digestive.Types
 import qualified Text.Digestive.Common as Common
 
+-- | Checks the input element when the argument is true
+--
+checked :: Bool -> Html -> Html
+checked False x = x
+checked True  x = x ! A.checked "checked"
+
 inputString :: (Monad m, Functor m)
             => Maybe String
             -> Form m String e Html String
-inputString defaultInput = Common.inputString defaultInput $ \id' inp ->
+inputString = Common.inputString $ \id' inp ->
     H.input ! A.type_ "text"
             ! A.name (H.stringValue $ show id')
             ! A.id (H.stringValue $ show id')
@@ -24,16 +31,15 @@ inputString defaultInput = Common.inputString defaultInput $ \id' inp ->
 inputRead :: (Monad m, Functor m, Show a, Read a)
           => Maybe a
           -> Form m String String Html a
-inputRead defaultInput =
-    Common.inputRead "No read" defaultInput $ \id' inp ->
-        H.input ! A.type_ "text"
-                ! A.name (H.stringValue $ show id')
-                ! A.id (H.stringValue $ show id')
-                ! A.value (H.stringValue $ fromMaybe "" inp)
+inputRead = flip Common.inputRead "No read" $ \id' inp ->
+    H.input ! A.type_ "text"
+            ! A.name (H.stringValue $ show id')
+            ! A.id (H.stringValue $ show id')
+            ! A.value (H.stringValue $ fromMaybe "" inp)
 
 inputPassword :: (Monad m, Functor m)
               => Form m String e Html String
-inputPassword = Common.inputString Nothing $ \id' inp ->
+inputPassword = flip Common.inputString Nothing $ \id' inp ->
     H.input ! A.type_ "password"
             ! A.name (H.stringValue $ show id')
             ! A.id (H.stringValue $ show id')
@@ -41,14 +47,27 @@ inputPassword = Common.inputString Nothing $ \id' inp ->
 
 inputBool :: (Monad m, Functor m)
           => Bool
-          -> Form m String String Html Bool
-inputBool defaultInput = Common.inputBool defaultInput $ \id' inp ->
+          -> Form m String e Html Bool
+inputBool inp = flip Common.inputBool inp $ \id' inp ->
     checked inp $ H.input ! A.type_ "checkbox"
                           ! A.name (H.stringValue $ show id')
                           ! A.id (H.stringValue $ show id')
+
+inputRadio :: (Monad m, Functor m, Eq a)
+           => Bool                        -- ^ Use @<br>@ tags
+           -> a                           -- ^ Default option
+           -> [(a, Html)]                 -- ^ Choices with their names
+           -> Form m String e Html a      -- ^ Resulting form
+inputRadio br def choices = Common.inputChoice toView def (map fst choices)
   where
-    checked False x = x
-    checked True  x = x ! A.checked "checked"
+    toView group id' sel val = do
+        checked sel $ H.input ! A.type_ "radio"
+                              ! A.name (H.stringValue $ show group)
+                              ! A.value (H.stringValue id')
+                              ! A.id (H.stringValue id')
+        H.label ! A.for (H.stringValue id')
+                $ fromMaybe mempty $ lookup val choices
+        when br H.br
 
 label :: Monad m
       => String
--- Text/Digestive/Common.hs
+++ Text/Digestive/Common.hs
@@ -2,8 +2,9 @@
 --
 module Text.Digestive.Common where
 
+import Control.Applicative ((<$>))
 import Control.Monad (mplus)
-import Data.Monoid (mempty)
+import Data.Monoid (Monoid, mempty, mconcat)
 import Data.Maybe (fromMaybe)
 
 import Text.Digestive.Types
@@ -11,10 +12,10 @@ import Text.Digestive.Types
 input :: (Monad m, Functor m)
       => (Bool -> Maybe String -> d -> s)           -- ^ Get the viewed result
       -> (Maybe String -> FormRange -> Result e a)  -- ^ Get the returned result
-      -> d                                          -- ^ Default value
       -> (FormId -> s -> v)                         -- ^ View constructor
+      -> d                                          -- ^ Default value
       -> Form m String e v a                        -- ^ Resulting form
-input toView toResult defaultInput createView = Form $ do
+input toView toResult createView defaultInput = Form $ do
     isInput <- isFormInput
     inp <- getFormInput
     id' <- getFormId
@@ -29,8 +30,8 @@ readMaybe string = case readsPrec 1 string of
     _ -> Nothing
 
 inputString :: (Monad m, Functor m)
-            => Maybe String                   -- ^ Default value
-            -> (FormId -> Maybe String -> v)  -- ^ View constructor
+            => (FormId -> Maybe String -> v)  -- ^ View constructor
+            -> Maybe String                   -- ^ Default value
             -> Form m String e v String       -- ^ Resulting form
 inputString = input toView toResult
   where
@@ -38,11 +39,11 @@ inputString = input toView toResult
     toResult = const . Ok . fromMaybe ""
 
 inputRead :: (Monad m, Functor m, Read a, Show a)
-          => e                              -- ^ Error when no read
+          => (FormId -> Maybe String -> v)  -- ^ View constructor
+          -> e                              -- ^ Error when no read
           -> Maybe a                        -- ^ Default input
-          -> (FormId -> Maybe String -> v)  -- ^ View constructor
           -> Form m String e v a            -- ^ Resulting form
-inputRead error' = input toView toResult
+inputRead cons' error' = input toView toResult cons'
   where
     toView _ inp def = inp `mplus` fmap show def
     toResult inp range = case readMaybe (fromMaybe "" inp) of
@@ -50,8 +51,8 @@ inputRead error' = input toView toResult
         Just y  -> Ok y
 
 inputBool :: (Monad m, Functor m)
-          => Bool                    -- ^ Default input
-          -> (FormId -> Bool -> v)   -- ^ View constructor
+          => (FormId -> Bool -> v)   -- ^ View constructor
+          -> Bool                    -- ^ Default input
           -> Form m String e v Bool  -- ^ Resulting form
 inputBool = input toView toResult
   where
@@ -59,6 +60,23 @@ inputBool = input toView toResult
     toResult inp _ = Ok $ readBool inp
     readBool (Just x) = not (null x)
     readBool Nothing  = False
+
+inputChoice :: (Monad m, Functor m, Monoid v, Eq a)
+            => (FormId -> String -> Bool -> a -> v)  -- ^ Choice constructor
+            -> a                                     -- ^ Default option
+            -> [a]                                   -- ^ Choices
+            -> Form m String e v a                   -- ^ Resulting form
+inputChoice toView defaultInput choices = Form $ do
+    inputKey <- fromMaybe "" <$> getFormInput
+    id' <- getFormId
+    let -- Find the actual input, based on the key, or use the default input
+        inp = fromMaybe defaultInput $ lookup inputKey $ zip (ids id') choices
+        -- Apply the toView' function to all choices
+        view' = mconcat $ zipWith (toView' id' inp) (ids id') choices
+    return (View (const view'), Ok inp)
+  where
+    ids id' = map (((show id' ++ "-") ++) . show) [1 .. length choices]
+    toView' id' inp key x = toView id' key (inp == x) x
 
 label :: Monad m
       => (FormId -> v)
