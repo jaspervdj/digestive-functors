7d8096985f687ad77a7ab5d57d5b0ebbe711f028
Jasper Van der Jeugt <jaspervdj@gmail.com>
2010-10-27 13:46:08 +0200

    Arrow instance for Transformer

--- Text/Digestive/Transformer.hs
+++ Text/Digestive/Transformer.hs
@@ -8,6 +8,7 @@ import Text.Digestive.Types
 import Control.Monad.Trans (lift)
 import Control.Monad ((<=<))
 import Control.Category (Category, (.), id)
+import Control.Arrow (Arrow, arr, first)
 
 newtype Transformer m e a b = Transformer
     { unTransformer :: a -> m (Either [e] b)
@@ -18,6 +19,12 @@ instance Monad m => Category (Transformer m e) where
     f . g = Transformer $ \x -> unTransformer g x >>= \x' -> case x' of
         Left e -> return $ Left e
         Right y -> unTransformer f y
+
+instance Monad m => Arrow (Transformer m e) where
+    arr f = Transformer $ return . Right . f
+    first t = Transformer $ \(x, y) -> unTransformer t x >>= \x' -> case x' of
+        Left e -> return $ Left e
+        Right x'' -> return $ Right (x'', y)
 
 transform :: Monad m => Form m i e v a -> Transformer m e a b -> Form m i e v b
 transform form transformer = Form $ do
