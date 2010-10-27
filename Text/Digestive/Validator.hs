a528c43b2b666c6b55801d5110cf3bca28ec06ce
Jasper Van der Jeugt <jaspervdj@gmail.com>
2010-10-27 14:06:14 +0200

    Rewrite Validator using Transformer

--- Text/Digestive/Validator.hs
+++ Text/Digestive/Validator.hs
@@ -2,38 +2,42 @@
 --
 module Text.Digestive.Validator where
 
+import Prelude hiding (id)
+
 import Control.Monad (liftM2)
 import Control.Monad.Trans (lift)
 import Data.Monoid (Monoid (..))
+import Control.Category (id)
+import Control.Applicative ((<$>))
 
 import Text.Digestive.Types
+import Text.Digestive.Transformer
 
--- | A validator
+-- | A validator. Invariant: the validator should not modify the result value,
+-- only check it.
 --
-newtype Validator m e a = Validator {unValidator :: a -> m [e]}
+newtype Validator m e a = Validator {unValidator :: Transformer m e a a}
 
 instance Monad m => Monoid (Validator m e a) where
-    mempty = Validator $ const $ return mempty
-    v1 `mappend` v2 = Validator $ \inp ->
-        liftM2 mappend (unValidator v1 inp) (unValidator v2 inp)
+    mempty = Validator id
+    v1 `mappend` v2 = Validator $ Transformer $ \inp ->
+        liftM2 eitherPlus (unTransformer (unValidator v1) inp)
+                          (unTransformer (unValidator v2) inp)
+      where
+        eitherPlus (Left e) (Left i) = Left $ e ++ i
+        eitherPlus (Left e) (Right _) = Left e
+        eitherPlus (Right _) (Left e) = Left e
+        eitherPlus (Right a) (Right _) = Right a
 
--- | Attach 'Validator's to a 'Form'
+-- | Attach a validator to a form.
 --
-validate :: Monad m => Form m i e v a -> [Validator m e a] -> Form m i e v a
-validate form validators = Form $ do
-    (view', result) <- unForm form
-    range <- getFormRange
-    -- Check the result
-    case result of
-        -- We already have an error, no more validation needed
-        Error e -> return (view', Error e)
-        -- Allright, now apply the validator
-        Ok x -> do
-            r <- lift $ lift $ unValidator (mconcat validators) x
-            return $ case r of
-                [] -> (view', Ok x)
-                -- Attach the range information to the errors
-                e -> (view', Error $ map ((,) range) e)
+validate :: Monad m => Form m i e v a -> Validator m e a -> Form m i e v a
+validate form = transform form . unValidator
+
+-- | Attach multiple validators to a form.
+--
+validateMany :: Monad m => Form m i e v a -> [Validator m e a] -> Form m i e v a
+validateMany form = validate form . mconcat
 
 -- | Easy way to create a pure validator
 --
@@ -49,6 +53,6 @@ checkM :: Monad m
        => e                -- ^ Error message
        -> (a -> m Bool)    -- ^ Actual validation
        -> Validator m e a  -- ^ Resulting validator
-checkM error' f = Validator $ \x -> do
+checkM error' f = Validator $ Transformer $ \x -> do
     valid <- f x
-    return $ if valid then [] else [error']
+    return $ if valid then Right x else Left [error']
