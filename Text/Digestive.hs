5810a94402093977fe79082b89345b65f83ed70a
Jasper Van der Jeugt <jaspervdj@gmail.com>
2010-10-30 14:39:09 +0200

    Add top module

---
+++ Text/Digestive.hs
@@ -0,0 +1,13 @@
+-- | Module re-exporting all core definitions
+--
+module Text.Digestive
+    ( module Text.Digestive.Result
+    , module Text.Digestive.Types
+    , module Text.Digestive.Transformer
+    , module Text.Digestive.Validator
+    ) where
+
+import Text.Digestive.Result
+import Text.Digestive.Types
+import Text.Digestive.Transformer
+import Text.Digestive.Validator
