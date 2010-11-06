9768228a557f7b440af6b8bf9fb9a7bd0d505608
Jasper Van der Jeugt <jaspervdj@gmail.com>
2010-11-06 13:33:17 +0100

    Add another layer to allow some HTML customization

---
+++ Text/Digestive/Html.hs
@@ -0,0 +1,62 @@
+-- | General functions for forms that are rendered to some sort of HTML
+module Text.Digestive.Html
+    ( FormHtmlConfig (..)
+    , FormHtml (..)
+    , applyClasses
+    , defaultHtmlConfig
+    , renderFormHtml
+    , renderFormHtmlWith
+    ) where
+
+import Data.Monoid (Monoid (..))
+import Data.List (intercalate)
+import Control.Applicative ((<*>), pure)
+
+-- | Settings for classes in generated HTML.
+--
+data FormHtmlConfig = FormHtmlConfig
+    { htmlInputClasses :: [String]  -- ^ Classes applied to input elements
+    , htmlLabelClasses :: [String]  -- ^ Classes applied to labels
+    , htmlErrorClasses :: [String]  -- ^ Classes applied to errors
+    } deriving (Show)
+
+-- | HTML describing a form
+--
+newtype FormHtml a = FormHtml
+    { unFormHtml :: FormHtmlConfig -> a
+    }
+
+instance Monoid a => Monoid (FormHtml a) where
+    mempty = FormHtml $ const mempty
+    mappend (FormHtml x) (FormHtml y) = FormHtml $ \c -> mappend (x c) (y c)
+
+-- | Apply all classes to an HTML element. If no classes are found, nothing
+-- happens.
+--
+applyClasses :: (a -> String -> a)            -- ^ Apply the class attribute
+             -> [FormHtmlConfig -> [String]]  -- ^ Labels to apply
+             -> FormHtmlConfig                -- ^ Label configuration
+             -> a                             -- ^ HTML element
+             -> a                             -- ^ Resulting element
+applyClasses applyAttribute fs cfg element = case concat (fs <*> pure cfg) of
+    []      -> element  -- No labels to apply
+    classes -> applyAttribute element $ intercalate " " classes
+
+-- | Default configuration
+--
+defaultHtmlConfig :: FormHtmlConfig
+defaultHtmlConfig = FormHtmlConfig
+    { htmlInputClasses = ["digestive-input"]
+    , htmlLabelClasses = ["digestive-label"]
+    , htmlErrorClasses = ["digestive-error"]
+    }
+
+-- | Render FormHtml using the default configuration
+--
+renderFormHtml :: FormHtml a -> a
+renderFormHtml = renderFormHtmlWith defaultHtmlConfig
+
+-- | Render FormHtml using a custom configuration
+--
+renderFormHtmlWith :: FormHtmlConfig -> FormHtml a -> a
+renderFormHtmlWith cfg = ($ cfg) . unFormHtml
