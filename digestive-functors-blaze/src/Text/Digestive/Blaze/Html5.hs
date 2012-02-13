29ba1bdab2d721e5c32cb6f14f58bcf936192dc3
Jasper Van der Jeugt <m@jaspervdj.be>
2012-02-13 15:22:49 +0100

    Add textarea, password, hidden

--- digestive-functors-blaze/src/Text/Digestive/Blaze/Html5.hs
+++ digestive-functors-blaze/src/Text/Digestive/Blaze/Html5.hs
@@ -1,5 +1,17 @@
 {-# LANGUAGE GADTs, OverloadedStrings #-}
-module Text.Digestive.Blaze.Html5 where
+module Text.Digestive.Blaze.Html5
+    ( inputText
+    , inputTextArea
+    , inputPassword
+    , inputHidden
+    , inputSelect
+    , inputRadio
+    , inputCheckbox
+    , inputSubmit
+    , label
+    , form
+    , errorList
+    ) where
 
 import Data.Monoid (mappend)
 import Control.Monad (forM_, when)
@@ -11,11 +23,6 @@ import qualified Text.Blaze.Html5.Attributes as A
 
 import Text.Digestive.View
 
-errorList :: Text -> View m Html -> Html
-errorList ref view = H.ul $ mapM_ H.li $ errors ref view
-
---------------------------------------------------------------------------------
-
 (!?) :: H.Attributable h => h -> (Bool, H.Attribute) -> h
 (!?) h (False, _) = h
 (!?) h (True,  a) = h ! a
@@ -26,6 +33,35 @@ label ref value = H.label ! A.for (H.toValue ref) $ value
 inputText :: Text -> View m v -> Html
 inputText ref view = H.input
     ! A.type_ "text"
+    ! A.id    (H.toValue ref)
+    ! A.name  (H.toValue ref)
+    ! A.value (H.toValue $ fieldTextInput ref view)
+
+inputTextArea :: Maybe Int    -- ^ Rows
+              -> Maybe Int    -- ^ Columns
+              -> Text         -- ^ Form path
+              -> View m Html  -- ^ View
+              -> Html         -- ^ Resulting HTML
+inputTextArea r c ref view = rows r $ cols c $ H.textarea
+    ! A.id    (H.toValue ref)
+    ! A.name  (H.toValue ref)
+    $ H.toHtml (fieldTextInput ref view)
+  where
+    rows (Just x) = (! A.rows (H.toValue x))
+    rows _        = id
+    cols (Just x) = (! A.cols (H.toValue x))
+    cols _        = id
+
+inputPassword :: Text -> View m v -> Html
+inputPassword ref view = H.input
+    ! A.type_ "password"
+    ! A.id    (H.toValue ref)
+    ! A.name  (H.toValue ref)
+    ! A.value (H.toValue $ fieldTextInput ref view)
+
+inputHidden :: Text -> View m v -> Html
+inputHidden ref view = H.input
+    ! A.type_ "hidden"
     ! A.id    (H.toValue ref)
     ! A.name  (H.toValue ref)
     ! A.value (H.toValue $ fieldTextInput ref view)
@@ -72,3 +108,6 @@ inputSubmit value = H.input
 
 form :: Text -> Html -> Html
 form action = H.form ! A.method "POST" ! A.action (H.toValue action)
+
+errorList :: Text -> View m Html -> Html
+errorList ref view = H.ul $ mapM_ H.li $ errors ref view
