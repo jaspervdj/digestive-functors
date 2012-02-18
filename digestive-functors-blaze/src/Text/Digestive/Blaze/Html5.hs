9e7ba8b53ced2531d19aeff4116c93c0b90c068a
Jasper Van der Jeugt <m@jaspervdj.be>
2012-02-18 17:33:04 +0100

    Be careful with absolute/relative ref's

--- digestive-functors-blaze/src/Text/Digestive/Blaze/Html5.hs
+++ digestive-functors-blaze/src/Text/Digestive/Blaze/Html5.hs
@@ -24,21 +24,31 @@ import Text.Blaze.Internal as H
 import qualified Text.Blaze.Html5 as H
 import qualified Text.Blaze.Html5.Attributes as A
 
+import Text.Digestive.Types
 import Text.Digestive.View
 
 (!?) :: H.Attributable h => h -> (Bool, H.Attribute) -> h
 (!?) h (False, _) = h
 (!?) h (True,  a) = h ! a
 
-label :: Text -> Html -> Html
-label ref value = H.label ! A.for (H.toValue ref) $ value
+absoluteRef :: Text -> View m v -> Text
+absoluteRef ref view = fromPath $ absolutePath ref view
+
+label :: Text -> View m v -> Html -> Html
+label ref view value = H.label
+    ! A.for (H.toValue ref')
+    $ value
+  where
+    ref' = absoluteRef ref view
 
 inputText :: Text -> View m v -> Html
 inputText ref view = H.input
     ! A.type_ "text"
-    ! A.id    (H.toValue ref)
-    ! A.name  (H.toValue ref)
+    ! A.id    (H.toValue ref')
+    ! A.name  (H.toValue ref')
     ! A.value (H.toValue $ fieldInputText ref view)
+  where
+    ref' = absoluteRef ref view
 
 inputTextArea :: Maybe Int    -- ^ Rows
               -> Maybe Int    -- ^ Columns
@@ -46,10 +56,11 @@ inputTextArea :: Maybe Int    -- ^ Rows
               -> View m Html  -- ^ View
               -> Html         -- ^ Resulting HTML
 inputTextArea r c ref view = rows r $ cols c $ H.textarea
-    ! A.id     (H.toValue ref)
-    ! A.name   (H.toValue ref)
+    ! A.id     (H.toValue ref')
+    ! A.name   (H.toValue ref')
     $ H.toHtml (fieldInputText ref view)
   where
+    ref'          = absoluteRef ref view
     rows (Just x) = (! A.rows (H.toValue x))
     rows _        = id
     cols (Just x) = (! A.cols (H.toValue x))
@@ -58,27 +69,32 @@ inputTextArea r c ref view = rows r $ cols c $ H.textarea
 inputPassword :: Text -> View m v -> Html
 inputPassword ref view = H.input
     ! A.type_ "password"
-    ! A.id    (H.toValue ref)
-    ! A.name  (H.toValue ref)
+    ! A.id    (H.toValue ref')
+    ! A.name  (H.toValue ref')
     ! A.value (H.toValue $ fieldInputText ref view)
+  where
+    ref' = absoluteRef ref view
 
 inputHidden :: Text -> View m v -> Html
 inputHidden ref view = H.input
     ! A.type_ "hidden"
-    ! A.id    (H.toValue ref)
-    ! A.name  (H.toValue ref)
+    ! A.id    (H.toValue ref')
+    ! A.name  (H.toValue ref')
     ! A.value (H.toValue $ fieldInputText ref view)
+  where
+    ref' = absoluteRef ref view
 
 inputSelect :: Text -> View m Html -> Html
 inputSelect ref view = H.select
-    ! A.id    (H.toValue ref)
-    ! A.name  (H.toValue ref)
+    ! A.id    (H.toValue ref')
+    ! A.name  (H.toValue ref')
     $ forM_ (zip choices [0 ..]) $ \(c, i) -> H.option
         !  A.value (value i)
         !? (i == idx, A.selected "selected")
         $ c
   where
-    value i        = H.toValue ref `mappend` "." `mappend` H.toValue i
+    ref'           = absoluteRef ref view
+    value i        = H.toValue ref' `mappend` "." `mappend` H.toValue i
     (choices, idx) = fieldInputChoice ref view
 
 inputRadio :: Bool         -- ^ Add @br@ tags?
@@ -87,30 +103,33 @@ inputRadio :: Bool         -- ^ Add @br@ tags?
            -> Html         -- ^ Resulting HTML
 inputRadio brs ref view = forM_ (zip choices [0 ..]) $ \(c, i) -> do
     let val = value i
-    H.input ! A.type_ "radio" ! A.value val ! A.id val ! A.name (H.toValue ref)
+    H.input ! A.type_ "radio" ! A.value val ! A.id val ! A.name (H.toValue ref')
         !? (i == idx, A.checked "checked")
     H.label ! A.for val $ c
     when brs H.br
   where
-    value i        = H.toValue ref `mappend` "." `mappend` H.toValue i
+    ref'           = absoluteRef ref view
+    value i        = H.toValue ref' `mappend` "." `mappend` H.toValue i
     (choices, idx) = fieldInputChoice ref view
 
 inputCheckbox :: Text -> View m Html -> Html
 inputCheckbox ref view = H.input
     !  A.type_ "checkbox"
-    !  A.id    (H.toValue ref)
-    !  A.name  (H.toValue ref)
+    !  A.id    (H.toValue ref')
+    !  A.name  (H.toValue ref')
     !? (selected, A.checked "checked")
   where
+    ref'     = absoluteRef ref view
     selected = fieldInputBool ref view
 
 inputFile :: Text -> View m Html -> Html
 inputFile ref view = H.input
     ! A.type_ "file"
-    ! A.id    (H.toValue ref)
-    ! A.name  (H.toValue ref)
+    ! A.id    (H.toValue ref')
+    ! A.name  (H.toValue ref')
     ! A.value (H.toValue value)
   where
+    ref'  = absoluteRef ref view
     value = fromMaybe "" $ fieldInputFile ref view
 
 inputSubmit :: Text -> Html
--- digestive-functors/src/Text/Digestive/View.hs
+++ digestive-functors/src/Text/Digestive/View.hs
@@ -10,6 +10,9 @@ module Text.Digestive.View
     , subView
 
       -- * Querying a view
+      -- ** Low-level
+    , absolutePath
+
       -- ** Form encoding
     , viewEncType
 
@@ -29,7 +32,6 @@ import Data.List (findIndex, isPrefixOf)
 import Data.Maybe (fromMaybe)
 
 import Data.Text (Text)
-import qualified Data.Text as T
 
 import Text.Digestive.Field
 import Text.Digestive.Form.Encoding
@@ -37,60 +39,57 @@ import Text.Digestive.Form.Internal
 import Text.Digestive.Types
 
 data View m v = forall a. View
-    { viewForm   :: Form m v a
-    , viewInput  :: [(Path, FormInput)]
-    , viewErrors :: [(Path, v)]
-    , viewMethod :: Method
+    { viewContext :: Path
+    , viewForm    :: Form m v a
+    , viewInput   :: [(Path, FormInput)]
+    , viewErrors  :: [(Path, v)]
+    , viewMethod  :: Method
     }
 
 instance Monad m => Functor (View m) where
-    fmap f (View form input errs method) = View
-        (formMapView f form) input (map (second f) errs) method
+    fmap f (View ctx form input errs method) = View
+        ctx (formMapView f form) input (map (second f) errs) method
 
 instance Show v => Show (View m v) where
-    show (View form input errs method) =
-        "View " ++ show form ++ " " ++ show input ++
+    show (View ctx form input errs method) =
+        "View " ++ show ctx ++ " " ++ show form ++ " " ++ show input ++
         " " ++ show errs ++ " " ++ show method
 
 getForm :: Form m v a -> View m v
-getForm form = View form [] [] Get
+getForm form = View [] form [] [] Get
 
 postForm :: Monad m => Form m v a -> Env m -> m (Either (View m v) a)
 postForm form env = eval Post env form >>= \(r, inp) -> return $ case r of
-    Error errs -> Left $ View form inp errs Post
+    Error errs -> Left $ View [] form inp errs Post
     Success x  -> Right x
 
 subView :: Text -> View m v -> View m v
-subView ref (View form input errs method) = case lookupForm (toPath ref) form of
-    (SomeForm f : _) -> View f input' errs' method
-    _                -> error $
-        "Text.Digestive.View.subView: " ++ T.unpack ref ++ " not found"
+subView ref (View ctx form input errs method) =
+    View (ctx ++ path) form input errs method
   where
-    path   = toPath ref
-    input' = [(p', i) | (p, i) <- input, p' <- dropPath p]
-    errs'  = [(p', e) | (p, e) <- errs, p' <- dropPath p]
+    path = toPath ref
 
-    dropPath xs
-        | path `isPrefixOf` xs = [drop (length path) xs]
-        | otherwise            = []
+-- | Determine an absolute 'Path' for a field in the form
+absolutePath :: Text -> View m v -> Path
+absolutePath ref (View ctx _ _ _ _) = ctx ++ toPath ref
 
 viewEncType :: View m v -> FormEncType
-viewEncType (View form _ _ _) = formEncType form
+viewEncType (View _ form _ _ _) = formEncType form
 
 lookupInput :: Path -> [(Path, FormInput)] -> [FormInput]
 lookupInput path = map snd . filter ((== path) . fst)
 
 fieldInputText :: Text -> View m v -> Text
-fieldInputText ref (View form input _ method) = fromMaybe "" $
+fieldInputText ref view@(View _ form input _ method) = fromMaybe "" $
     queryField path form $ \field -> case field of
         Text t -> Just $ evalField method givenInput (Text t)
         _      -> Nothing
   where
-    path       = toPath ref
+    path       = absolutePath ref view
     givenInput = lookupInput path input
 
 fieldInputChoice :: Text -> View m v -> ([v], Int)
-fieldInputChoice ref (View form input _ method) = fromMaybe ([], 0) $
+fieldInputChoice ref view@(View _ form input _ method) = fromMaybe ([], 0) $
     queryField path form $ \field -> case field of
         Choice xs i -> do
             let x = evalField method givenInput (Choice xs i)
@@ -98,30 +97,31 @@ fieldInputChoice ref (View form input _ method) = fromMaybe ([], 0) $
             return (map snd xs, idx)
         _           -> Nothing
   where
-    path       = toPath ref
+    path       = absolutePath ref view
     givenInput = lookupInput path input
 
 fieldInputBool :: Text -> View m v -> Bool
-fieldInputBool ref (View form input _ method) = fromMaybe False $
+fieldInputBool ref view@(View _ form input _ method) = fromMaybe False $
     queryField path form $ \field -> case field of
         Bool x -> Just $ evalField method givenInput (Bool x)
         _      -> Nothing
   where
-    path       = toPath ref
+    path       = absolutePath ref view
     givenInput = lookupInput path input
 
 fieldInputFile :: Text -> View m v -> Maybe FilePath
-fieldInputFile ref (View form input _ method) =
+fieldInputFile ref view@(View _ form input _ method) =
     queryField path form $ \field -> case field of
         File -> evalField method givenInput File
         _    -> Nothing
   where
-    path       = toPath ref
+    path       = absolutePath ref view
     givenInput = lookupInput path input
 
 errors :: Text -> View m v -> [v]
-errors ref = map snd . filter ((== toPath ref) . fst) . viewErrors
+errors ref view = map snd $ filter ((== absolutePath ref view) . fst) $
+    viewErrors view
 
 childErrors :: Text -> View m v -> [v]
-childErrors ref =
-    map snd . filter ((toPath ref `isPrefixOf`) . fst) . viewErrors
+childErrors ref view = map snd $
+    filter ((absolutePath ref view `isPrefixOf`) . fst) $ viewErrors view
