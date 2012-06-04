c83cb5aade4ea04177677704168f6af3448d941a
Jasper Van der Jeugt <m@jaspervdj.be>
2012-06-04 09:39:29 +0200

    Add a Ref module

--- digestive-functors/src/Text/Digestive.hs
+++ digestive-functors/src/Text/Digestive.hs
@@ -3,11 +3,13 @@
 module Text.Digestive
     ( module Text.Digestive.Form
     , module Text.Digestive.Form.Encoding
+    , module Text.Digestive.Ref
     , module Text.Digestive.Types
     , module Text.Digestive.View
     ) where
 
-import Text.Digestive.Form
-import Text.Digestive.Form.Encoding
-import Text.Digestive.Types
-import Text.Digestive.View
+import           Text.Digestive.Form
+import           Text.Digestive.Form.Encoding
+import           Text.Digestive.Ref
+import           Text.Digestive.Types
+import           Text.Digestive.View
---
+++ digestive-functors/src/Text/Digestive/Ref.hs
@@ -0,0 +1,31 @@
+--------------------------------------------------------------------------------
+-- | This module contains utilities for creating text fragments to identify
+-- forms.
+{-# LANGUAGE OverloadedStrings #-}
+module Text.Digestive.Ref
+    ( makeRef
+    , makeRefs
+    ) where
+
+
+--------------------------------------------------------------------------------
+import qualified Data.ByteString    as B
+import           Data.Text          (Text)
+import qualified Data.Text          as T
+import qualified Data.Text.Encoding as T
+import           Text.Printf        (printf)
+
+
+--------------------------------------------------------------------------------
+-- | Convert an arbitrary text value (possibly containing spaces, dots etc. to
+-- a text value that safely be used as an identifier in forms.
+makeRef :: Text -> Text
+makeRef =
+    -- We simply UTF-8 encode and then hex encode, so all characters are valid.
+    T.append "df-" . T.pack . (printf "%02x" =<<) . B.unpack . T.encodeUtf8
+
+
+--------------------------------------------------------------------------------
+-- | Create an infinite list of refs.
+makeRefs :: [Text]
+makeRefs = ["df-" `T.append` T.pack (show i) | i <- [0 :: Int ..]]
